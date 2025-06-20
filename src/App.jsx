51b54e89310b2f99e08e402ca648490830e5a82b
Sushey01 <susmagar012@gmail.com>
2025-06-20 14:58:05 +0545

    login-feri-bana

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import Navbar from './pages/Navbar'
 import HeroSection from './pages/HeroSection'
 import Pricing from './pages/Pricing'
 import Product from './pages/Product'
 
 const App = () => {
   return (
-    <div>
-      <HeroSection/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import "./Navbar.css"
+import { Link } from 'react-router-dom'
+import "../pages/Navbar.css"
+import Logo from "../assets/images/worldwise.png"
 
 const Navbar = () => {
   return (
-     <div className="nav-title">
-          <div className="nav-logo">
-            <img src={Logo} alt="logo" />
-            <div className="nav">
-              <a href="#">Pricing</a>
-              <a href="#">Product</a>
-              <button className="login-button">LOGIN</button>
-            </div>
-          </div>
-          </div>
+    <div className="nav-title">
+      <div className="nav-logo">
+        <img src={Logo} alt="logo" />
+        <div className="nav">
+          <Link to="/pricing">Pricing</Link>
+          <Link to="/product">Product</Link>
+          
+          <Link to="/login">
+            <button className="login-button">LOGIN</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
