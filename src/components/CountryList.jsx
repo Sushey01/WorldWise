b2ac1e026a58f9e0977642df740c1c3d263a6414
Sushey01 <susmagar012@gmail.com>
2025-06-23 17:08:56 +0545

    done

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -5,32 +5,38 @@ import styles from "../components/CountryList.module.css";
 const CountryList = () => {
     // const { country, dispatch} = useCities();
   const { cities } = useCities();
-  console.log(cities)
+//   console.log(cities)
 
-  const getCountries = (countries) => {
-    dispatch ({type: "GET_CITY", payload: countries})
-  }
+//   const getCountries = (countries) => {
+//     dispatch ({type: "GET_CITY", payload: countries})
+//   }
 
   // Get unique countries from the cities list
   const countries = [...new Set(cities.map((city) => city.country))];
 
-const coumtries = cities.reduce((arr, city)=>{
-    if (!arr.includes(city.country)) return [...arr, city.country];
-    else return arr;
-}, [])
+
+
+  //Reduce Method used for extracting countries unique
+// const countries = cities.reduce((arr, city)=>{
+//     if (!arr.includes(city.country)) return [...arr, city.country];
+//     else return arr;
+// }, [])
+
+ if (!cities.length)
+    return (
+      <p className={styles.message}>
+        👋 Add your first city by clicking on a city on the map
+      </p>
+    );
+
 
   return (
-    <div className={styles.countryList}>
-        {/* <h1>SPAIN</h1> */}
-      {countries.length === 0 ? (
-        <p className={styles.message}>
-                👋 Add your first city by clicking on a city on the map
-              </p>
-      ) : (
-        countries.map((country, index) => (
-          <span key={index}>{country}</span>
-        ))
-      )}
+   <div className={styles.countryList}>
+      {countries.map((country, index) => (
+        <div key={index} className={styles.countryCard}>
+          <span className={styles.countryName}>{country || "Unknown Country"}</span>
+        </div>
+      ))}
     </div>
   );
 };
