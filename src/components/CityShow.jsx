fdf7557269cf4e83078053bc3f8eb54f595824d6
Sushey01 <susmagar012@gmail.com>
2025-06-24 23:58:51 +0545

    completed-ui

--- src/components/CityShow.jsx
+++ src/components/CityShow.jsx
@@ -12,11 +12,11 @@ const CityShow = () => {
         <p>LEARN MORE</p>
         <div className={styles.wiki}>
         <a href='#'>Check out on Wikipedia</a>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" color="#FFB545gi" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
         </div>
       </div>
       <button className={styles.click}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-left-icon lucide-move-left"><path d="M6 8L2 12L6 16"/><path d="M2 12H22"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color='#ececec' viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-left-icon lucide-move-left"><path d="M6 8L2 12L6 16"/><path d="M2 12H22"/></svg>
         <h3>BACK</h3>
       </button>
     </div>
